9c2ecc55bbf4acc8e01db851e80c726c99f49597
lumierax <lumierax@gmail.com>
2026-09-26 12:59:21 +0300

    Delete install_hidden.bat

--- install_hidden.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process PowerShell -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0install_hidden.ps1""'"
